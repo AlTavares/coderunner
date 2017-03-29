448206cd2625e47d0176d0fb38d11644f832f0d6
Alexandre Mantovani Tavares <alexandre@live.in>
2017-03-29 18:25:01 -0300

    feature(runner): add success return value

--- runner/runner.go
+++ runner/runner.go
@@ -9,6 +9,16 @@ import (
 	"time"
 )
 
+var Icons = struct {
+	success string
+	failure string
+	info    string
+}{
+	" ✅ ",
+	" 🔴 ",
+	" ℹ️  ",
+}
+
 func Run(program string, args []string) (string, time.Duration, error) {
 	fmt.Println("\nCommand:", program, args)
 	start := time.Now()
@@ -20,28 +30,29 @@ func Run(program string, args []string) (string, time.Duration, error) {
 	return strings.TrimSpace(fmt.Sprintf("%s", out)), timeElapsed, err
 }
 
-func Test(program string, args []string, input string, expectedOutput string, expectedExecutionTime time.Duration) {
+func Test(program string, args []string, input string, expectedOutput string, expectedExecutionTime time.Duration) (success bool) {
 	args = append(args, input)
 	output, duration, err := Run(program, args)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var icon = " ✅ "
+	var icon = Icons.failure
 
-	if output != expectedOutput {
-		icon = " 🔴 "
+	if output == expectedOutput {
+		icon = Icons.success
+		success = true
 	}
 	fmt.Println(icon, "Expected "+expectedOutput+", got "+output)
 
 	if expectedExecutionTime > 0 {
-		icon = " ✅ "
+		icon = Icons.success
 		if duration > expectedExecutionTime {
-			icon = " 🔴 "
+			icon = Icons.failure
 		}
 		fmt.Println(icon, "Executed in", duration, "the maximum expected was", expectedExecutionTime)
 		return
 	}
 
-	fmt.Println(" ℹ️  Executed in", duration)
-
+	fmt.Println(Icons.info, "Executed in", duration)
+	return
 }
